491f3bc0d6a52c4e63dd3ff8433f9ea19ddc78b6
KishanNandha <kishan.nandha99@gmail.com>
2020-05-05 22:47:16 +0530

    Added Builder Pattern

---
+++ src/main/java/CreationalDesignPatterns/builder/UserDTO.java
@@ -0,0 +1,11 @@
+package CreationalDesignPatterns.builder;
+
+//Interface implemented by "products"
+public interface UserDTO {
+	
+	String getName();
+
+	String getAddress();
+
+	String getAge();
+}
